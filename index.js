55a082919295e920462b9bda3826f9b7bda0eb15
Arbaz khan <79788148+Sahil-6757@users.noreply.github.com>
2024-10-16 17:27:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,71 +10,49 @@ app.use(cors());
 app.use(bodyparser.json());
 // Middleware ends
 
-
-
 async function main() {
-    await mongoose.connect(
-      "mongodb+srv://arbaz151033:Arbazkhan%406757@cluster.dapmmwg.mongodb.net/Newsuploader?retryWrites=true&w=majority&appName=clusternp"
-    );
-  }
-  main().catch((err) => console.log(err));
+  await mongoose.connect(
+    "mongodb+srv://arbaz151033:Arbazkhan%406757@cluster.dapmmwg.mongodb.net/Newsuploader?retryWrites=true&w=majority&appName=clusternp"
+  );
+}
+main().catch((err) => console.log(err));
 
 const newSchema = new mongoose.Schema({
-    name: { type: String, unique: false },
-    description: { type: String, unique: false },
-    imageurl: { type: String, unique: false },
-    date: { type: String, unique: false },
-  
-  });
+  name: { type: String, unique: false },
+  description: { type: String, unique: false },
+  imageurl: { type: String, unique: false },
+  date: { type: String, unique: false },
+});
 
-  const indiannewSchema = new mongoose.Schema({
-    name: { type: String, unique: false },
-    description: { type: String, unique: false },
-    imageurl: { type: String, unique: false },
-    date: { type: String, unique: false },
-  });
+const indiannewSchema = new mongoose.Schema({
+  name: { type: String, unique: false },
+  description: { type: String, unique: false },
+  imageurl: { type: String, unique: false },
+  date: { type: String, unique: false },
+});
 
-  let News = mongoose.model("newsUploader", newSchema);
-  let indNews = mongoose.model("indianNews", indiannewSchema);
+let News = mongoose.model("newsUploader", newSchema);
+let indNews = mongoose.model("indianNews", indiannewSchema);
 
-  app.post("/formpost", async (req, res) => {
-    let news = new News();
-    news.name = await req.body.title
-    news.description = await req.body.description
-    news.imageurl = await req.body.imageUrl
-    news.date = await req.body.date
-    await news.save();
-    res.json({message:"Success"})
-  });
+app.post("/formpost", async (req, res) => {
+  let news = new News();
+  news.name = await req.body.title;
+  news.description = await req.body.description;
+  news.imageurl = await req.body.imageUrl;
+  news.date = await req.body.newDate;
+  await news.save();
+  res.json({ message: "Success" });
+});
 
- app.post("/indianpost", async (req, res) => {
-    let news = new indNews();
-    news.name = await req.body.title
-    news.description = await req.body.description
-    news.imageurl = await req.body.imageUrl
-    news.date = await req.body.newsDate
-    await news.save();
-    res.json({message:"Success"})
-  });
-
-  app.delete("/formdelete/:id", async (req, res) => {
-    try {
-      let id = await req.params.id;
-      let data = await News.findById({ _id: id });
-      
-      if (!data.$isEmpty()) {
-        await News.deleteOne({ _id: id });
-        res.json({ message: "Deleted" });
-      } else {
-        res.json({ message: "No data Found" });
-      }
-      res.json({message:"Success"})
-    } catch (error) {
-      console.log(error)
-    }
-   
-  });
-
+app.post("/indianpost", async (req, res) => {
+  let news = new indNews();
+  news.name = await req.body.title;
+  news.description = await req.body.description;
+  news.imageurl = await req.body.imageUrl;
+  news.date = await req.body.newDate;
+  await news.save();
+  res.json({ message: "Success" });
+});
 
 app.delete("/indianNewsdelete/:id", async (req, res) => {
   try {
@@ -92,15 +70,16 @@ app.delete("/indianNewsdelete/:id", async (req, res) => {
   }
 });
 
-app.get("/formget",async (req,res)=>{
-    let result = await News.find({});
-    res.json(result)
-})
+app.get("/formget", async (req, res) => {
+  let result = await News.find({});
+  res.json(result);
+});
 
 app.get("/indianNews", async (req, res) => {
   let result = await indNews.find({});
   res.json(result);
 });
+
 app.listen(8000, () => {
   console.log("server on http://localhost:8000");
 });
