ab94f1ac1b13c880f97dddc3604db4296957daa9
Sahil Khan <Arbaz151033@gmail.com>
2024-10-27 18:13:45 +0530

    new backend

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const cors = require("cors");
 const bodyparser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
+const multer = require("multer")
+const path = require("path")
+const cloudinary = require('cloudinary').v2;
+const fs = require("fs")
+
 
 // Middleware starts
 app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
+// app.use(express.static("server/uploads"));
 // Middleware ends
 
 async function main() {
@@ -16,6 +22,29 @@ async function main() {
   );
 }
 main().catch((err) => console.log(err));
+
+console.log(path.join(__dirname,'./uploads'))
+
+    // Configuration
+    cloudinary.config({ 
+      cloud_name: 'dbi2w9wjw', 
+      api_key: '316433984532898', 
+      api_secret: 'Yjv9ZcE-fVJgnaydoBFqjpr8M04' // Click 'View API Keys' above to copy your API secret
+  });
+
+const storage = multer.diskStorage({
+  destination: function(req,file,cb){
+    cb(null,path.join(__dirname,'./uploads'))
+  },
+  filename: async function(req,file,cb){
+    cb(null,`${Date.now()+'-'+file.fieldname+'.jpg'}`)
+   
+  }
+})
+const upload = multer({ storage })
+
+
+
 
 const newSchema = new mongoose.Schema({
   name: { type: String, unique: false },
@@ -34,13 +63,18 @@ const indiannewSchema = new mongoose.Schema({
 let News = mongoose.model("newsUploader", newSchema);
 let indNews = mongoose.model("indianNews", indiannewSchema);
 
-app.post("/formpost", async (req, res) => {
+app.post("/formpost", upload.single("file"), async (req, res) => {
+
+  const response = await cloudinary.uploader.upload(req.file.path)
+  console.log(response)
+  console.log(req.file.path)
   let news = new News();
   news.name = await req.body.title;
   news.description = await req.body.description;
-  news.imageurl = await req.body.imageUrl;
+  news.imageurl = response.url
   news.date = await req.body.newDate;
   await news.save();
+  fs.unlinkSync(req.file.path)
   res.json({ message: "Success" });
 });
 
@@ -99,3 +133,10 @@ app.get("/indianNews", async (req, res) => {
 app.listen(8000, () => {
   console.log("server on http://localhost:8000");
 });
+
+
+
+
+// await v2.uploader.upload(path.join(__dirname,'./uploads')+file.fieldname+'.jpg').catch((error)=>{
+//   console.log("clodunnary error "+error)
+// })
